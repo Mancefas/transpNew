4d855613c367a1aca1089849cf5103ec056c5025
Mancefas <mantvydasliobe@gmail.com>
2021-11-16 09:35:07 +0200

    en text added, logo change

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const copyEmailToClipboard = () => {
     });
   copiedDiv.insertAdjacentHTML(
     "afterbegin",
-    '<div class="alert alert-success" role="alert">Email nukopijuotas. Galite įklijuoti į gavėjo laukelį </div>'
+    '<div class="alert alert-success" role="alert">Email nukopijuotas.</div>'
   );
 
   emailBtn.innerText = "transpeda@transpeda.lt";
@@ -27,6 +27,39 @@ const copyEmailToClipboard = () => {
 
   setTimeout(() => {
     copiedDiv.remove(0);
+  }, 3000);
+};
+
+// ENG version copied email
+const copiedDivEng = document.getElementById("copied-eng");
+const emailBtnEng = document.getElementById("email-contact-eng");
+
+const copyEmailToClipboardEng = () => {
+  if (!navigator.clipboard) {
+    // Clipboard API not available
+    return;
+  }
+  const emailAdress = document.getElementById("email").innerHTML;
+
+  navigator.clipboard
+    .writeText(emailAdress)
+    .then(() => {
+      console.log("Text copied to clipboard...");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+  copiedDivEng.insertAdjacentHTML(
+    "afterbegin",
+    '<div class="alert alert-success" role="alert">Email copied</div>'
+  );
+
+  emailBtnEng.innerText = "transpeda@transpeda.lt";
+  emailBtnEng.classList.remove("btn-lg");
+  emailBtnEng.classList.remove("px-4");
+
+  setTimeout(() => {
+    copiedDivEng.remove(0);
   }, 3000);
 };
 
